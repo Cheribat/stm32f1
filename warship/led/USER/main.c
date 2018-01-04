9834da1c6816834514ae193624b4a1db66e9da87
Cheribat <yuchengliang1011@126.com>
2018-01-04 22:59:06 +0800

    beep test ok

--- warship/led/USER/main.c
+++ warship/led/USER/main.c
@@ -2,6 +2,7 @@
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
+#include "beep.h"
 
 int main(void)
 {		
@@ -9,15 +10,18 @@ int main(void)
     
     delay_init();	    	  //延时函数初始化	  
     NVIC_Configuration(); 	  //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
-//    uart_init(9600);	      //串口初始化为9600
+    uart_init(9600);	      //串口初始化为9600
     LED_Init();
+    BEEP_Init();
     
     while(1)
     {
-//        printf("t:%d\n",t);
+        printf("t:%d\n",t);
         LED0 = 1;
+//        BEEP = 1;
         delay_ms(300);
         LED0 = 0;
+        BEEP = 0;
         LED1 = 1;
         delay_ms(300);
         LED1 = 0;

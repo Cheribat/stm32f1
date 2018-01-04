6bdb470e3d3e5d7ab6dbccb51ff79e7eb8908bb1
Cheribat <yuchengliang1011@126.com>
2018-01-04 23:10:06 +0800

    key test ok

--- warship/led/USER/main.c
+++ warship/led/USER/main.c
@@ -3,6 +3,7 @@
 #include "usart.h"
 #include "led.h"
 #include "beep.h"
+#include "key.h"
 
 int main(void)
 {		
@@ -13,20 +14,33 @@ int main(void)
     uart_init(9600);	      //串口初始化为9600
     LED_Init();
     BEEP_Init();
+    KEY_Init();
     
-    while(1)
-    {
-        printf("t:%d\n",t);
-        LED0 = 1;
-//        BEEP = 1;
-        delay_ms(300);
-        LED0 = 0;
-        BEEP = 0;
-        LED1 = 1;
-        delay_ms(300);
-        LED1 = 0;
-        t++;
-    }
+	while(1)
+	{
+ 		t=KEY_Scan(0);		
+        
+	   	if(t)
+		{						   
+			switch(t)
+			{				 
+				case KEY_UP:	
+					BEEP=!BEEP;
+					break;
+				case KEY_LEFT:	
+					LED0=!LED0;
+					break;
+				case KEY_DOWN:
+					LED1=!LED1;
+					break;
+				case KEY_RIGHT:
+					LED0=!LED0;
+					LED1=!LED1;
+					break;
+			}
+		}else 
+            delay_ms(10); 
+	}	
 }
 
 /* END OF FILE */
